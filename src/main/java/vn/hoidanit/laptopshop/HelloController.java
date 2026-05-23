7eea1747989213f7427fc3244d1ebbe51d6425ec
GiaBaoo <ngbao840@gmail.com>
2026-05-23 11:14:57 +0700

    config mysql, security & disable security

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -9,4 +9,14 @@ public class HelloController {
 		return "Hello World from Spring Boot!";
 	}
 
+	@GetMapping("/user")
+	public String userPage() {
+		return "Only user can access this page!";
+	}
+
+	@GetMapping("/admin")
+	public String adminPage() {
+		return "Only admin can access this page!";
+	}
+
 }
