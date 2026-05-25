037ad44075db2126b365815c8ed96ab84f34cdea
GiaBaoo <ngbao840@gmail.com>
2026-05-25 20:06:15 +0700

    config link to css

--- src/main/java/vn/hoidanit/laptopshop/config/WebMvcConfig.java
+++ src/main/java/vn/hoidanit/laptopshop/config/WebMvcConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -28,4 +29,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
   public void configureViewResolvers(ViewResolverRegistry registry) {
     registry.viewResolver(viewResolver());
   }
+
+  // Config link css
+  @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("/resources/css/");
+    }
+
 }
